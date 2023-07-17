e121a798ee036263bc2af1cb84234a71cf7e2324
Lino S. Goncalves <lino.g@outlook.com>
2023-07-17 23:34:01 +0200

    added import statements

---
+++ QC-Tutorial.py
@@ -0,0 +1,4 @@
+# Import libraries required for research.
+from QuantConnect.Research import QuantBook
+from QuantConnect import Resolution
+from QuantConnect.Indicators import ExponentialMovingAverage
